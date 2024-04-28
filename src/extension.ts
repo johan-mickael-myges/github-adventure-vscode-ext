1f4928124ca6104b91d434f79c687c6789d66e41
Johan Mickaël <johan.rakotoniaina@mevia.fr>
2024-04-28 22:46:03 +0200

    Fix invalid URL for map

--- src/extension.ts
+++ src/extension.ts
@@ -111,7 +111,7 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 
 			gitUrl = btoa(gitUrl);
-			let mapUrl = `${process.env.WA_HOST || 'https://play.workadventu.re'}/_/${gitUrl}/${process.env.GIT_ADVENTURE_MAP_API_HOST || 'localhost:8877'}/map/${gitUrl}`;
+			let mapUrl = `${process.env.WA_HOST || 'https://play.workadventu.re'}/_/${gitUrl}/${process.env.GIT_ADVENTURE_MAP_API_HOST || 'localhost:8877'}/${gitUrl}`;
 			await goToGithubAdventureMapCTA(mapUrl);
 		});
 	});
